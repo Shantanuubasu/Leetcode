ea4e4d58eb757813a833eecbfce51a0f065b9788
Shantanu Basu <basu.shantanu2003@gmail.com>
2024-05-02 17:18:00 +0530

    Update 2441. Largest Positive Integer That Exists With Its Negative.cpp

--- Array/2441. Largest Positive Integer That Exists With Its Negative.cpp
+++ Array/2441. Largest Positive Integer That Exists With Its Negative.cpp
@@ -26,3 +26,36 @@ public:
 3. Then we check if the current elemnent's negation is present in the array or not.
 4. If yes update `mx` if it is greater.
 5. Finally return `mx`. */
+
+
+//APPROACH-2-TWO POINTERS
+
+class Solution {
+public:
+    int findMaxK(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        sort(nums.begin(),nums.end());
+
+        int i=0,j=nums.size()-1;
+        int ans=-1;
+        while(i<j){
+            if(-nums[i]==nums[j] && abs(nums[i])>ans){
+                ans=abs(nums[j]);
+            }
+            else if(-nums[i]<nums[j]){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+
+        return ans;
+    }
+};
+
+//----------------------------------------------------------------------
+/* 1. Sort the array `nums`.
+2. Take two pointers and start checking if numbers match the condition.
+3. If yes, update the `ans` variable.
+4. Return `ans`. */
