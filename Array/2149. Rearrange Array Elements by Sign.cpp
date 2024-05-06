c8d8dfc750ea456df116000b62dccdd20491b44a
Shantanu Basu <basu.shantanu2003@gmail.com>
2024-05-06 21:30:14 +0530

    Update 2149. Rearrange Array Elements by Sign.cpp

--- Array/2149. Rearrange Array Elements by Sign.cpp
+++ Array/2149. Rearrange Array Elements by Sign.cpp
@@ -39,3 +39,30 @@ Now after we obtain both the arrays, we will merge both accordingly.
 Inside the while loop, we will first insert a positive element and then a negative element Finally, we will return the res array. */
 
 //APPROACH-2-USING-ONE-ARRAY
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        ios_base::sync_with_stdio(false); cin.tie(NULL);
+        int n=nums.size();
+        vector<int> res(n,0);
+        int x=0,y=1;
+        for(int i=0; i<n; ++i){
+            if(nums[i]>=0){
+                res[x]=nums[i];
+                x+=2;
+            }
+            else{
+                res[y]=nums[i];
+                y+=2;
+            }
+        }
+        return res;
+
+    }
+};
+
+//--------------------------------------------------------------------------
+/* 1. We will add the elements to another array according to their value.
+2. We will take one pointer to start from 0 which will add all the positive element in the array.
+3. Another pointer will start from 11 which will add all the negative elements in the array. */
