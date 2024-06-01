edebd3ea276f520c4616d1ed78bb651c04eba649
Shantanu Basu <basu.shantanu2003@gmail.com>
2024-06-01 13:32:19 +0530

    Create 2769. Find the Maximum Achievable Number.cpp

---
+++ Math/2769. Find the Maximum Achievable Number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        return num+(t*2);
+    }
+};
+
+//---------------------------------------------------------
+/* 1. Well did not understand what the problem statement is asking us to do but guessed the solution looking at the example testcase and guess what it is the right solution. */
