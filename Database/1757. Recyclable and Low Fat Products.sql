8b8e1d88edf47b2685a8961edd22015924e387b6
Shantanu Basu <basu.shantanu2003@gmail.com>
2024-06-12 12:09:27 +0530

    Create 1757. Recyclable and Low Fat Products.sql

---
+++ Database/1757. Recyclable and Low Fat Products.sql
@@ -0,0 +1,7 @@
+/* Write your MySQL query statement below */
+select product_id
+from Products
+where low_fats='Y' AND recyclable='Y'
+
+/* --------------------------------------------------- 
+1. We will select product_id from Products table, where low_fats and recyclable should be 'Y'. */
