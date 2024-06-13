39162860230d5fcdb5eacd811497dfde1fb67b64
Shantanu Basu <basu.shantanu2003@gmail.com>
2024-06-13 11:59:09 +0530

    Create 2469. Convert the Temperature.cpp

---
+++ Math/2469. Convert the Temperature.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        double kelvin= celsius+273.15;
+        double fahrenheit= (celsius*1.80) + 32.00;
+
+        return {kelvin,fahrenheit};
+    }
+};
+
+//-----------------------------------------------------------------
+/* 1. Calculate kelvin and fahrenheit using the formula given.
+2. Return both. */
