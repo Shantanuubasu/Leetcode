9d92facdee76a4b1110a8dbbbcf9114b375811fa
Shantanu Basu <basu.shantanu2003@gmail.com>
2024-06-21 16:03:39 +0530

    Create 2396. Strictly Palindromic Number.cpp

---
+++ Math/2396. Strictly Palindromic Number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        return false;
+    }
+};
+
+//---------------------------------------------------------
+/* 1. For every number n, its value in n-2 is 12.
+2. So for every number the answer will be false.
+3. So, just return false. */
