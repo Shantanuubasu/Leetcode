bb2ca7383d8da5f893303e8d02f9ebe915989b91
Shantanu Basu <basu.shantanu2003@gmail.com>
2024-06-25 11:18:43 +0530

    Create 1038. Binary Search Tree to Greater Sum Tree.cpp

---
+++ Tree/1038. Binary Search Tree to Greater Sum Tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum=0;
+    void reverseInorder(TreeNode* root){
+        if(root==NULL){
+            return;
+        }
+        reverseInorder(root->right);
+        sum+=root->val;
+        root->val=sum;
+        reverseInorder(root->left);
+    }
+    TreeNode* bstToGst(TreeNode* root) {
+        ios_base::sync_with_stdio(false);
+
+        reverseInorder(root);
+        return root;
+    }
+};
+
+//---------------------------------------------------------
+/* 1. Take a sum global variable to keep track of sum of values of nodes.
+2. Now we will apply reverse inorder, so we will first go to the right subtree and add its value to the sum and then update node value to sum.
+3. Then we will go to the left subtree and repeat the process.
+4. Finally return root. */
