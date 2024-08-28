4597ca280305cd837caba0b4941801b9b9820fd5
Shantanu Basu <basu.shantanu2003@gmail.com>
2024-08-28 18:57:48 +0530

    Update 199. Binary Tree Right Side View.cpp

--- Tree/199. Binary Tree Right Side View.cpp
+++ Tree/199. Binary Tree Right Side View.cpp
@@ -1,3 +1,5 @@
+//APPROACH - 1 - ITERATIVE 
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -58,3 +60,45 @@ public:
 5. Now we will add the left and right child of n to q2. This q2 will act as a waiting queue which will store the elements in the next level.
 6. So now after the ending each level we will move contents of q2 to q1.
 7. Finally, return ans. */
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//APPROACH - 2 - RECURSIVE
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void solve(TreeNode* root, vector<int>& res, int level){
+        if(root==NULL){
+            return;
+        }
+
+        if(level==res.size()){
+            res.push_back(root->val);
+        }
+
+        solve(root->right, res, level+1);
+        solve(root->left, res, level+1);
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        solve(root, res, 0);
+        return res;
+    }
+};
+
+//--------------------------------------------------------------------------------------
+/* 1. We will traverse the right subtree first, so in solve(), if root is null then return.
+2. If level (level of node in tree) is same as res's size then push the root's val in res.
+3. Now go towards right and increment level by 1.
+4. Also go towards left and increment level by 1. */
