ddce45fa21cc17a795241cfaf7eec0e074b0a561
Shantanu Basu <basu.shantanu2003@gmail.com>
2024-09-04 10:32:59 +0530

    Update 94. Binary Tree Inorder Traversal.cpp

--- Tree/94. Binary Tree Inorder Traversal.cpp
+++ Tree/94. Binary Tree Inorder Traversal.cpp
@@ -1,3 +1,4 @@
+// APPROACH - 1 - RECURSIVE 
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -31,3 +32,53 @@ public:
 3. Then we push the node val to v.
 4. Then we explore the right subtree.
 5. Finally, return v. */
+//--------------------------------------------------------------------------
+
+//APPROACH - 2 - MORRIS TRAVERSAL
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        TreeNode* curr=root;
+
+        while(curr!=NULL){
+            if(curr->left==NULL){
+                res.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                TreeNode* leftchild=curr->left;
+                while(leftchild->right!=NULL){
+                    leftchild=leftchild->right;
+                }
+
+                leftchild->right=curr;
+                TreeNode* temp=curr;
+                curr=curr->left;
+                temp->left=NULL;
+            }
+        }
+
+        return res;
+    }
+};
+
+//--------------------------------------------------------------
+/* 1. To traverse the binary tree in inorder morris traversal we will convert the tree into threaded binary tree. Where the last node is connected to the root.
+2. Take curr as root and if curr's left is NULL then push curr's val to res and move curr to right.
+3. If left is not NULL then take a leftchild pointer starting from left of curr and move it to extreme right.
+4. Assign leftchild's right to curr and take a temp pointer and assign curr to it.
+5. Now move curr to its left child and make temp's left as NULL.
+6. Finally, return res. */
